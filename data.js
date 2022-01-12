2d5c0655fdc454388f10d0746645eaf8a302cf92
davidemontagna <davidemontagna92@gmail.com>
2022-01-12 15:11:26 +0100

    ins. le box con colori (milestone 2 completa)

--- data.js
+++ data.js
@@ -1,114 +1,133 @@
-[
+const cards = [
 	{
 		name: 'cat',
 		prefix: 'fa-',
 		type: 'animal',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'orange'
 	},
 	{
 		name: 'crow',
 		prefix: 'fa-',
 		type: 'animal',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'orange'
 	},
 	{
 		name: 'dog',
 		prefix: 'fa-',
 		type: 'animal',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'orange'
 	},
 	{
 		name: 'dove',
 		prefix: 'fa-',
 		type: 'animal',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'orange'
 	},
 	{
 		name: 'dragon',
 		prefix: 'fa-',
 		type: 'animal',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'orange'
 	},
 	{
 		name: 'horse',
 		prefix: 'fa-',
 		type: 'animal',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'orange'
 	},
 	{
 		name: 'hippo',
 		prefix: 'fa-',
 		type: 'animal',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'orange'
 	},
 	{
 		name: 'fish',
 		prefix: 'fa-',
 		type: 'animal',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'orange'
 	},
 	{
 		name: 'carrot',
 		prefix: 'fa-',
 		type: 'vegetable',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'green'
 	},
 	{
 		name: 'apple-alt',
 		prefix: 'fa-',
 		type: 'vegetable',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'green'
 	},
 	{
 		name: 'lemon',
 		prefix: 'fa-',
 		type: 'vegetable',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'green'
 	},
 	{
 		name: 'pepper-hot',
 		prefix: 'fa-',
 		type: 'vegetable',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'green'
 	},
 	{
 		name: 'user-astronaut',
 		prefix: 'fa-',
 		type: 'user',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'blue'
 	},
 	{
 		name: 'user-graduate',
 		prefix: 'fa-',
 		type: 'user',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'blue'
 	},
 	{
 		name: 'user-ninja',
 		prefix: 'fa-',
 		type: 'user',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'blue'
 	},
 	{
 		name: 'user-secret',
 		prefix: 'fa-',
 		type: 'user',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'blue'
 	}
 ];
+
+//richiamo la funzione per stampare le cards
+printCards(cards)
+
+
+//funzione per stampare le cards
+function printCards(myObj){
+
+	const cardsContainer = document.getElementById("dm-container-cards");
+
+	myObj.forEach((element, index) => {
+		cardsContainer.innerHTML += `
+			<div class="mt-5 mx-5 dm-box d-flex flex-column justify-content-center align-items-center">
+				<i class="${element.family} ${element.prefix}${element.name} dm-icon-${element.color}"></i>
+				<div class="title">${element.name}</div>
+			</div>
+		`
+	});
+}
